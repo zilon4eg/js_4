6caa3caaf996a2499a9e256e264845f7cebd4065
Igor <hdd500t@yandex.ru>
2022-04-24 16:53:20 +0300

    complete

--- hw3.js
+++ hw3.js
@@ -1,45 +1,95 @@
-function getReadline() {
-    return require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-}
-
-async function getAnswer(question) {
-
-    const readline = getReadline();
-    readline.question(question, answer => {
-        let msg = answer;
-        readline.close()
-        return msg;
-    });
-
+function addCounter() {
+    let counter = 1;
+    return function() {
+        counter++;
+        return counter;
+    }
 }
 
 
+const rl = require('readline');
+const { promisify } = require('util');
+const readline = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+// Prepare readline.question for promisification
+readline.question[promisify.custom] = (question) => {
+    return new Promise((resolve) => {
+        readline.question(question, resolve);
+    });
+};
+
+
+async function answerAnalysis(answer) {
+    answer = answer.replace(/^\s+|\s+$/g, '')
+
+    if (!answer) {
+        let answer = await promisify(readline.question)(`\nВведено пустое поле, попытка не засчитана.\nПопробуйте еще разок: `);
+        await answerAnalysis(answer)
+    }
+
+    else if (!isNaN(answer)) {
+
+        if (hiddenNumber > answer) {
+            count = counter();
+            let answer = await promisify(readline.question)(`\nПопытка №${count}\nМое число БОЛЬШЕ!\nПопробуйте еще разок: `);
+            await answerAnalysis(answer)
+        }
+        else if (hiddenNumber < answer) {
+            count = counter();
+            let answer = await promisify(readline.question)(`\nПопытка №${count}\nМое число МЕНЬШЕ!\nПопробуйте еще разок: `);
+            await answerAnalysis(answer)
+        }
+        else {
+            let answer = await promisify(readline.question)(`\nПоздравляю, вы угадали!\nПотрачено попыток: ${count}\nСыграем еще (да/нет)? `);
+            await isTheEnd(answer)
+        }
+
+    }
+
+  else {
+      count = counter();
+      let answer = await promisify(readline.question)(`\nПопытка №${count}\nВведено не число!\nПопробуйте еще разок: `);
+      await answerAnalysis(answer)
+  }
+}
+
+
+async function isTheEnd(answer) {
+    let yes = ['yes', 'y', 'да', 'д'];
+    let no = ['no', 'n', 'нет', 'н'];
+
+    if (yes.includes(answer.toLowerCase())) {
+        await startGame()
+    }
+    else if (no.includes(answer.toLowerCase())) {
+        console.log('\nОтлично поиграли, до встречи!');
+        readline.close();
+    }
+    else {
+        console.log('\nОтвета не понял ¯\\_(ツ)_/¯, до встречи!');
+        readline.close();
+    }
+}
+
+
+// Usage example:
 async function startGame() {
-    
-    let question = 'Как тебя зовут? '
-    let answer = await getAnswer(question);
-    console.log(`Привет, ${answer}`)
 
-}
+    hiddenNumber = Math.floor(Math.random() * 101);
+    // console.log(hiddenNumber);
+    counter = addCounter();
+    count = 1;
+    let answer = await promisify(readline.question)('\nЯ загадал число от 0 до 100.\nУгадай, какое число я загадал (Попытка №1): ');
+    await answerAnalysis(answer);
 
+};
+
+
+var hiddenNumber;
+var counter;
+var count;
 startGame();
-
-
-
-//////////////////////////////////////////////////////
-//  этот код работает корректно, но не получается
-//  вывести в переменную результат пользовательского ввода
-
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// readline.question('Как тебя зовут? ', answer => {
-//     console.log(`Привет, ${answer}!`);
-//     readline.close();
-// });
-//////////////////////////////////////////////////////
